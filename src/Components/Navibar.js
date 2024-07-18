4f41a242a0cc213939e76ad0acb1bf3ee8dfcac3
Tanya Katkova <yndx-katkovatanya@yandex.ru>
2024-07-18 20:18:20 +0300

    fix: navbar

--- src/Components/Navibar.js
+++ src/Components/Navibar.js
@@ -1,25 +1,39 @@
 import React from "react";
-import { Nav, Navbar, Button } from "react-bootstrap";
+import { Nav, Navbar, Button, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Styles = styled.div`
+a, .navbar-brand, navbar-nav .nav-link {
+color: #adb1b8;
+text-decoration: none;
+&:hover {
+color: white
+}}
+`
 
 export default function NaviBar() {
   return (
     <>
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand>Tanya Katkova</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link><Link to="/">Home</Link></Nav.Link>
-            <Nav.Link><Link to="/users">Users</Link></Nav.Link>
-            <Nav.Link><Link to="/about">About</Link></Nav.Link>
-          </Nav>
-          <Nav>
-            <Button variant="primary" className="me-2">Log In</Button>
-            <Button variant="primary">Sign Out</Button>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <Styles>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand>Tanya Katkova</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link><Link to="/">Home</Link></Nav.Link>
+                <Nav.Link><Link to="/users">Users</Link></Nav.Link>
+                <Nav.Link><Link to="/about">About</Link></Nav.Link>
+              </Nav>
+              <Nav>
+                <Button variant="primary" className="me-2">Log In</Button>
+                <Button variant="primary">Sign Out</Button>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </Styles>
     </>
   )
 }
